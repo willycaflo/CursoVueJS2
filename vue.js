aec9c1e591132505ac1a29a08275cef7fcc7aa96
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-18 11:48:51 -0600

    Enlazar datos a una propiedad de un elemento html

--- vue.js
+++ vue.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '.vue',
     data: {
-        mensaje: 'Aprende Vue.js Fácilmente'
+        mensaje: 'Aprende Vue.js Fácilmente',
+        src: 'https://vuejs.org/images/logo.png'
     },
     methods:{
         mostrarMensaje: function(){
