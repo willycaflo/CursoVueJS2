0a38aa3b3328ded824899e958dd35f25762c11a4
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-18 12:25:45 -0600

    Ganchos de ciclo de vida de una instacia de Vue

--- vue.js
+++ vue.js
@@ -1,12 +1,35 @@
-var app = new Vue({
-    el: '.vue',
+new Vue({
+    el: '#vm',
     data: {
-        mensaje: 'Aprende Vue.js Fácilmente',
-        src: 'https://vuejs.org/images/logo.png'
+        mensaje: 'Este es el mensaje',
     },
-    methods:{
-        mostrarMensaje: function(){
-            return this.mensaje;
+    beforeCreate: function () {
+        console.log('Llamando beforeCreate');
+    },
+    created: function() {
+        console.log('Llamando created');
+    },
+    beforeMount: function(){
+        console.log('Llamando beforeMount');
+    },
+    mounted: function() {
+        console.log('Llamando mounted');
+    },
+    beforeUpdate: function(){
+        console.log('Llamando beforeUpdate');
+    },
+    updated: function(){
+        console.log('Llamando updated');
+    },
+    beforeDestroy: function() {
+        console.log('Llamando beforeDestroy');
+    },
+    destroyed: function() {
+        console.log('Llamando destroyed');
+    },
+    methods: {
+        destruir: function(){
+            this.$destroy();
         }
-    }
+    },
 });
