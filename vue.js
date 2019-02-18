ffd4870f624202412b000cbeb784da080b6eb859
Guillermo Cabrera <guillermo.cabrera@skyangel.com.mx>
2019-02-18 11:44:25 -0600

    Ejemplo de como realizar una interpolacion de metodos

--- vue.js
+++ vue.js
@@ -2,5 +2,10 @@ var app = new Vue({
     el: '.vue',
     data: {
         mensaje: 'Aprende Vue.js Fácilmente'
+    },
+    methods:{
+        mostrarMensaje: function(){
+            return this.mensaje;
+        }
     }
 });
